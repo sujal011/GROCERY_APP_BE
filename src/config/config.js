e64399b12f137340be0c517e104d3f9f9401fc8f
Sujal Bhavsar <sujalofficial011@gmail.com>
2025-03-09 18:55:13 +0530

    port 80

--- src/config/config.js
+++ src/config/config.js
@@ -4,7 +4,7 @@ import {Admin} from "../models/index.js";
 import "dotenv/config";
 
 
-export const PORT = process.env.PORT || 3000;   
+export const PORT = process.env.PORT || 80;   
 export const COOKIE_SECRET = process.env.COOKIE_SECRET
 
 const MongoDBStore = ConnectMongoDBSession(fastifySession);
